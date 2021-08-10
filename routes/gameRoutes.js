a3721d552e90e3c8fb4884ebf7886154255af8ec
DataPlant <dataplant@outlook.com>
2021-08-10 19:15:06 -0400

    formdata is submitted and stored, then outputted in gamepage as a list with links

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../models/index.js')
+const db = require('../models/index.js');
 
 router.get('/', (req, res) => {
-    res.render('game/game.ejs')
-})
+    db.Game.find({}, (err, allGame) => {
+        if(err) return console.log(err);
+        res.render('game/game.ejs', {
+            allGame: allGame
+        })
+    })
+});
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.redirect('/game');
-})
+    console.log(req.body);
+    let data = {
+        title: req.body.title,
+        summary: req.body.summary,
+    };
+    db.Game.create(data, (err, createdGame) => {
+        if(err) return console.log(err);
+        res.redirect('/game')
+    })
+    // res.redirect('/game');
+});
+// router.get('/:id', (req, res) => {
+//     console.log(req.params.id)
+    
 
+// })
 
 module.exports = router;
